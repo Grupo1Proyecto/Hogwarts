85fea85c8329075fadaf4208cf03797619e86504
Grupo1POO <33700504+Grupo1ProyectoPOO@users.noreply.github.com>
2017-11-15 16:06:33 -0500

    Update Persona.java
    get y setters

--- PROYECTO/src/Proyecto/Persona.java
+++ PROYECTO/src/Proyecto/Persona.java
@@ -22,6 +22,63 @@ public class Persona {
     private String Animal;
     private String Hechizo;
     
+        // get y setters
+    }
+    public String getNombre(){
+    return this.Nombre;
+    }
+    public void setNombre(String Nombre){
+    this.Nombre=Nombre;
+    }
+
+    public String getApellido(){
+    return this.Apellido;
+    }
+    public void setApellido(String Apellido){
+    this.Apellido=Apellido;
+    }
+    public int getEdad(){
+    return this.Edad;
+    }
+    public void setEdad(int Edad){
+    this.Edad=Edad;
+    }
+    public String getVarita(){
+    return this.Varita;
+    }
+    public void setVarita(String Varita){
+    this.Varita=Varita;
+    }
+    public String getDeporte(){
+    return this.Deporte;
+    }
+    public void setDeporte(String Deporte){
+    this.Deporte=Deporte;
+    }
+    public String getEscoba(){
+    return this.Escoba;
+    }
+    public void setEscoba(String Escoba){
+    this.Escoba=Escoba;
+    }
+    public String getPocion(){
+    return this.Pocion;
+    }
+    public void setPocion(String Pocion){
+    this.Pocion=Pocion;
+    }
+    public String getAnimal(){
+    return this.Animal;
+    }
+    public void setAnimal(String Animal){
+    this.Animal=Animal;
+    }
+    public String getHechizo(){
+    return this.Hechizo;
+    }
+    public void setHechizo(String Hechizo){
+    this.Hechizo=Hechizo;
+    }
     
     
 }
