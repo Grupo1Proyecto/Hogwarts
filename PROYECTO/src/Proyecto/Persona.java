95dfa08e8afce02e80cfadf351d1902d24d6a406
Admin <Admin@GGCIBLABWRK084.espol.edu.ec>
2017-11-15 14:59:26 -0500

    Creacion de clases de los tipos de magos.

---
+++ PROYECTO/src/Proyecto/Persona.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Proyecto;
+
+/**
+ *
+ * @author Admin
+ */
+public class Persona {
+    private String Nombre;
+    private String Apellido;
+    private int Edad;
+    private String Casa;
+    private String Tipo;
+    private String varita;
+    private String Deporte;
+    private String Escoba;
+    private String Pocion;
+    private String Animal;
+    private String Hechizo;
+    
+    
+    
+}
