cd21848778c48ffbec8191d9a4ec44a8fdd07787
Luis Iván Ramírez <luiirami@github.com>
2017-12-01 18:38:48 -0500

    interface volar y despegar

--- PROYECTO/src/Proyecto/Metamorfomago.java
+++ PROYECTO/src/Proyecto/Metamorfomago.java
@@ -9,6 +9,16 @@ package Proyecto;
  *
  * @author Admin
  */
-public class Metamorfomago {
+public class Metamorfomago extends Persona implements Volador {
+
+    @Override
+    public String volar() {
+        return "Vuelo con mi poder de transformarme en animal";
+    }
+
+    @Override
+    public String despegar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
